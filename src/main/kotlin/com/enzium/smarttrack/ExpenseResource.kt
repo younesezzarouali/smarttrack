09b2929c27b5d73cff43064119b76b5e6433a222
younes_ezzarouali <youness.ezzarouali@gmail.com>
2026-02-28 01:19:22 +0100

    upgrade: java 21, quarkus 3.32.1, kotlin 2.3.10

--- src/main/kotlin/com/enzium/smarttrack/ExpenseResource.kt
+++ src/main/kotlin/com/enzium/smarttrack/ExpenseResource.kt
@@ -5,8 +5,6 @@ import jakarta.transaction.Transactional
 import jakarta.ws.rs.GET
 import jakarta.ws.rs.POST
 import jakarta.ws.rs.Path
-import jakarta.ws.rs.Produces
-import jakarta.ws.rs.core.MediaType
 import jakarta.ws.rs.core.Response
 
 @Path("/expenses")
